563211e7e5df5da8b7922e8f3f74b4ed75871a34
Huangnli <50835276+Huangnli@users.noreply.github.com>
2023-05-06 09:41:19 -0400

    Exercício 0 - Done -preenche os campos obrigatórios e envia o formulário

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,11 +1,30 @@
 // <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    //Verificar o titulo do ./src/index.html
-    it('verifica o título da aplicação', function() {
+describe('Central de Atendimento ao Cliente TAT', function () {
+
+    // runs before every test block (root-level hook)
+    beforeEach(() => {
         cy.visit('./src/index.html')
-        
+    })
+
+    it('verifica o título da aplicação', function () {
         cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
     })
-  })
-  
+
+    // # = id
+    // . = class
+    it.only('preenche os campos obrigatórios e envia o formulário', function () {
+        //Montagem do cenário
+
+        //Execução
+        cy.get('#firstName').type('Julio')
+        cy.get('#lastName').type('Huang')
+        cy.get('#email').type('Julio@huang.com')
+        cy.get('#open-text-area').type('Estou com duvidas sobre...')
+        cy.get('button[type="submit"]').click()
+
+        //Verificação
+        cy.get('.success').should('be.visible')
+
+    })
+})
