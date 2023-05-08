4f34bb87d70a5f322d03bf2725bbeb8c419dc1f1
Huangnli <50835276+Huangnli@users.noreply.github.com>
2023-05-08 17:42:11 -0400

    Exercício extra -marca cada tipo de atendimento

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -133,10 +133,18 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
     })
 
-    it.only('marca o tipo de atendimento "Feedback"', function () {
+    it('marca o tipo de atendimento "Feedback"', function () {
         /*======Execução e Verificação======*/
         cy.get('#support-type [type="radio"][value="feedback"]').check().should('have.value', 'feedback')
 
     })
 
+    it.only('marca cada tipo de atendimento', function () {
+        /*======Execução e Verificação======*/
+        cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(function ($radio) {
+                cy.wrap($radio).check().should('be.checked')
+            })
+    })
 })
