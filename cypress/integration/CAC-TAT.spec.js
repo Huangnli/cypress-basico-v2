2f568f5342ebbaae9fe50e9bba31fc8434679572
Huangnli <50835276+Huangnli@users.noreply.github.com>
2023-05-09 17:01:23 -0400

    Exercício extra 1 Done -seleciona um arquivo simulando um drag-and-drop

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -167,12 +167,21 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('.error').should('be.visible')
     })
 
-    it.only('seleciona um arquivo da pasta fixtures', function () {
+    it('seleciona um arquivo da pasta fixtures', function () {
         cy.get('input#file-upload')
             .should('not.have.value')
             .selectFile('./cypress/fixtures/example.json')
             .should(function ($input) {
                 // console.log($input)
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    it.only('seleciona um arquivo simulando um drag-and-drop', function () {
+        cy.get('input#file-upload')
+            .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+            .should(function ($input) {
                 expect($input[0].files[0].name).to.equal('example.json')
             })
     })
